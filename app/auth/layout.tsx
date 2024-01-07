4c20d8c33061e585dc9da01c213c4513857810f4
metinsut <metindir@gmail.com>
2024-01-07 20:30:24 +0300

    add forms

---
+++ app/auth/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-600">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
